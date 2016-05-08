79acc9184d35a6493056a1c0fe67bb57eae41261
worldcreate <furatsuki_fiskm@yahoo.co.jp>
2016-05-08 18:37:19 +0900

    add create symlink

---
+++ create_symlink.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+cd $(dirname $0)
+git submodule init
+git submodule foreach 'git pull origin master'
+for dotfile in .?*; do
+    case $dotfile in
+        *.elc)
+            continue;;
+        ..)
+            continue;;
+        .git)
+            continue;;
+        .vimperator)
+            ln -Fis "$PWD/$dotfile/plugin" $HOME/$dotfile/plugin
+            ;;
+        *)
+            ln -Fis "$PWD/$dotfile" $HOME
+            ;;
+    esac
+done
