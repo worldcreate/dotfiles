bcdc2fdfc2456894a2d428d28fe9a7d4934d9a3a
worldcreate <furatsuki_fiskm@yahoo.co.jp>
2016-10-16 17:44:11 +0900

    gitとcurlの場合分け

--- etc/init/vim-setup.sh
+++ etc/init/vim-setup.sh
@@ -1,9 +1,22 @@
 #/bin/bash
 
+is_exists() {
+	which "$1" >/dev/null 2>&1
+	return $?
+}
+
 bundle_path=$HOME/dotfiles/.vim/bundle
 if [ ! -e $bundle_path ]; then
 	mkdir -p $bundle_path
 fi
 
 cd $bundle_path
-git clone https://github.com/Shougo/neobundle.vim
+
+if is_exists "git"; then
+	git clone https://github.com/Shougo/neobundle.vim
+elif is_exists "curl"; then
+	curl https://github.com/Shougo/neobundle.vim/dotfiles/archive/master.tar.gz | tar zxv
+else
+	echo "neobundleのインストールに失敗"
+	exit 1
+fi
